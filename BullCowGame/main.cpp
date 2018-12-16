fc185ae1d4a1d1ca7b2b3610e387158112671768
fekitibi <tiborfek7@gmail.com>
2018-12-16 16:58:53 +0100

    Adding the project

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	constexpr int WORD_LENGTH = 5;
+
+	// introduce the game
+	cout << "Welcome to Bulls and Cows game"<< endl;
+	cout << "Could you guess the " << WORD_LENGTH << " length isogram?\n";
+
+	// get a guess from the player
+
+	// repeat the guess back to them
+	return 0;
+}
