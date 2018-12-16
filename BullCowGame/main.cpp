0ec0d42f1437987c80c673055d5ca105ad631601
fekitibi <tiborfek7@gmail.com>
2018-12-16 17:59:10 +0100

    Added PrintIntro and GetGuess functions

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,22 +3,39 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuess();
+
+// the entry point to our application
 int main() {
-	constexpr int WORD_LENGTH = 5;
 
-	// introduce the game
-	cout << "Welcome to Bulls and Cows game"<< endl;
-	cout << "Could you guess the " << WORD_LENGTH << " length isogram?\n";
-
-	// get a guess from the player
-	cout << "Enter your guess: ";
-	string Guess = "";
-	getline(cin, Guess);
+	PrintIntro();
 
 	// repeat the guess back to them
-	cout << "Your guess was: " << Guess;
+	cout << "Your guess was: " << GetGuess();
 
 
 	cout << endl;
 	return 0;
+}
+
+// introduce the game
+void PrintIntro() {
+
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to Bulls and Cows game" << endl;
+	cout << "Could you guess the " << WORD_LENGTH << " length isogram?" << endl;
+
+	return;
+}
+
+// get a guess from the player
+string GetGuess() {
+	
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+
+	return Guess;
 }
