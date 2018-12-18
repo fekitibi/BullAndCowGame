79842d1c1c367ffbd16ef805412c49d95869a3b1
fekitibi <tiborfek7@gmail.com>
2018-12-18 19:42:13 +0100

    BullCow struct added hidden word added to the header and the class(FBullCowGame)

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -8,7 +8,9 @@ int32 FBullCowGame::GetCurrentTry() const { return CurrentTry; }
 void FBullCowGame::Reset(){
 
 	constexpr int32 MAX_TRIES = 8;
-
+	const FString HIDDEN_WORD = "planet";
+	
+	HiddenWord = HIDDEN_WORD;
 	MaxTry = MAX_TRIES;
 	CurrentTry = 1;
 }
@@ -19,4 +21,15 @@ bool FBullCowGame::IsGameWon() const{
 
 bool FBullCowGame::CheckGuessValidity(FString Guess){
 	return false;
+}
+
+// recives valid guess, increment try and return bulls and cows
+BullCowCount FBullCowGame::SubmitGuess(FString guess){
+	// setup return variable
+	BullCowCount BullCowCount;
+	//increment try number
+	CurrentTry++;
+	//check all letters to the hidden word
+	//return bulls and cows
+	return BullCowCount;
 }
--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -4,6 +4,12 @@
 using FString = std::string;
 using int32 = int;
 
+struct BullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
 class FBullCowGame{
 
 public:
@@ -15,10 +21,12 @@ public:
 
 	void Reset(); // TODO: later could return something
 	bool CheckGuessValidity(FString Guess); // TODO: later could return something
-	// TODO provide method for bul and cow counting and increase try number
+	//bull and cow counting and increase try number method assuming valid guess
+	BullCowCount SubmitGuess(FString guess);
 
 private:
 	// see constructor for initialization
 	int32 CurrentTry;
 	int32 MaxTry;
+	FString HiddenWord;
 };
